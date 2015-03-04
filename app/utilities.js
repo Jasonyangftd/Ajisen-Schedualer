66efedd7696312aad487e794c5dae97412c97bcb
Jason Yang <jasonyang@Jasons-MacBook-Air.local>
2015-03-03 19:06:20 -0800

    deploying

--- app/utilities.js
+++ app/utilities.js
@@ -1,112 +1,121 @@
 var conflicts = []
+
+var mondayA ={'name': 'Monday'};
+var tuesdayA ={'name': 'Tuesday'};
+var wendnesdayA ={'name': 'Wendnesday'};
+var thursdayA ={'name': 'Thursday'};
+var fridayA ={'name': 'Friday'};
+var saturdayA ={'name': 'Saturday'};
+var sundayA ={'name': 'Sunday'};
+
+var innerFuncA = function(){
+  var temp = [];
+  $('.monday option:selected').each(function() {
+    temp.push($(this).val());
+  });
+  for (var i = 0; i < temp.length; i++){
+    if (mondayA[temp[i]]){
+      mondayA[temp[i]]++;
+    } else {
+      mondayA[temp[i]] = 1;
+    }
+  }
+};
+
+var innerFuncB = function(){
+  var temp = [];
+  $('.tuesday option:selected').each(function() {
+    temp.push($(this).val());
+  });
+  for (var i = 0; i < temp.length; i++){
+    if (tuesdayA[temp[i]]){
+      tuesdayA[temp[i]]++;
+    } else {
+      tuesdayA[temp[i]] = 1;
+    }
+  }
+};
+
+var innerFuncC = function(){
+  var temp = [];
+  $('.wendnesday option:selected').each(function() {
+    temp.push($(this).val());
+  });
+  for (var i = 0; i < temp.length; i++){
+    if (wendnesdayA[temp[i]]){
+      wendnesdayA[temp[i]]++;
+    } else {
+      wendnesdayA[temp[i]] = 1;
+    }
+  }
+};
+
+var innerFuncD = function(){
+  var temp = [];
+  $('.thursday option:selected').each(function() {
+    temp.push($(this).val());
+  });
+  for (var i = 0; i < temp.length; i++){
+    if (thursdayA[temp[i]]){
+      thursdayA[temp[i]]++;
+    } else {
+      thursdayA[temp[i]] = 1;
+    }
+  }
+};
+
+var innerFuncE = function(){
+  var temp = [];
+  $('.friday option:selected').each(function() {
+    temp.push($(this).val());
+  });
+  for (var i = 0; i < temp.length; i++){
+    if (fridayA[temp[i]]){
+      fridayA[temp[i]]++;
+    } else {
+      fridayA[temp[i]] = 1;
+    }
+  }
+};
+
+var innerFuncF = function(){
+  var temp = [];
+  $('.saturday option:selected').each(function() {
+    temp.push($(this).val());
+  });
+  for (var i = 0; i < temp.length; i++){
+    if (saturdayA[temp[i]]){
+      saturdayA[temp[i]]++;
+    } else {
+      saturdayA[temp[i]] = 1;
+    }
+  }
+};
+
+var innerFuncG = function(){
+  var temp = [];
+  $('.sunday option:selected').each(function() {
+    temp.push($(this).val());
+  });
+  for (var i = 0; i < temp.length; i++){
+    if (sundayA[temp[i]]){
+      sundayA[temp[i]]++;
+    } else {
+      sundayA[temp[i]] = 1;
+    }
+  }
+};
+
 var checkCon = function(){
-  var mondayA ={'name': 'Monday'};
-  var tuesdayA ={'name': 'Tuesday'};
-  var wendnesdayA ={'name': 'Wendnesday'};
-  var thursdayA ={'name': 'Thursday'};
-  var fridayA ={'name': 'Friday'};
-  var saturdayA ={'name': 'Saturday'};
-  var sundayA ={'name': 'Sunday'};
-  
-  var innerFuncA = function(){
-    var temp = [];
-    $('.monday option:selected').each(function() {
-      temp.push($(this).val());
-    });
-    for (var i = 0; i < temp.length; i++){
-      if (mondayA[temp[i]]){
-        mondayA[temp[i]]++;
-      } else {
-        mondayA[temp[i]] = 1;
-      }
-    }
-  };
+  conflicts = [];
 
-  var innerFuncB = function(){
-    var temp = [];
-    $('.tuesday option:selected').each(function() {
-      temp.push($(this).val());
-    });
-    for (var i = 0; i < temp.length; i++){
-      if (tuesdayA[temp[i]]){
-        tuesdayA[temp[i]]++;
-      } else {
-        tuesdayA[temp[i]] = 1;
-      }
-    }
-  };
-
-  var innerFuncC = function(){
-    var temp = [];
-    $('.wendnesday option:selected').each(function() {
-      temp.push($(this).val());
-    });
-    for (var i = 0; i < temp.length; i++){
-      if (wendnesdayA[temp[i]]){
-        wendnesdayA[temp[i]]++;
-      } else {
-        wendnesdayA[temp[i]] = 1;
-      }
-    }
-  };
-
-  var innerFuncD = function(){
-    var temp = [];
-    $('.thursday option:selected').each(function() {
-      temp.push($(this).val());
-    });
-    for (var i = 0; i < temp.length; i++){
-      if (thursdayA[temp[i]]){
-        thursdayA[temp[i]]++;
-      } else {
-        thursdayA[temp[i]] = 1;
-      }
-    }
-  };
-
-  var innerFuncE = function(){
-    var temp = [];
-    $('.friday option:selected').each(function() {
-      temp.push($(this).val());
-    });
-    for (var i = 0; i < temp.length; i++){
-      if (fridayA[temp[i]]){
-        fridayA[temp[i]]++;
-      } else {
-        fridayA[temp[i]] = 1;
-      }
-    }
-  };
-
-  var innerFuncF = function(){
-    var temp = [];
-    $('.saturday option:selected').each(function() {
-      temp.push($(this).val());
-    });
-    for (var i = 0; i < temp.length; i++){
-      if (saturdayA[temp[i]]){
-        saturdayA[temp[i]]++;
-      } else {
-        saturdayA[temp[i]] = 1;
-      }
-    }
-  };
-
-  var innerFuncG = function(){
-    var temp = [];
-    $('.sunday option:selected').each(function() {
-      temp.push($(this).val());
-    });
-    for (var i = 0; i < temp.length; i++){
-      if (sundayA[temp[i]]){
-        sundayA[temp[i]]++;
-      } else {
-        sundayA[temp[i]] = 1;
-      }
-    }
-  };
-
-
+  mondayA ={'name': 'Monday'};
+  tuesdayA ={'name': 'Tuesday'};
+  wendnesdayA ={'name': 'Wendnesday'};
+  thursdayA ={'name': 'Thursday'};
+  fridayA ={'name': 'Friday'};
+  saturdayA ={'name': 'Saturday'};
+  sundayA ={'name': 'Sunday'};
 
   innerFuncA()
   innerFuncB()
@@ -132,12 +141,9 @@ var checkCon = function(){
   check(fridayA);
   check(saturdayA);
   check(sundayA);
-  // console.log(mondayA)
 
-  console.log(conflicts)
+  this.showConflicts = conflicts;
 }
-
-// checkCon()
 
 
 
